8743835cf14b0ac727a310adb8fbc18e38e9bd1a
Suzie Hoops <suziehoops17@gmail.com>
2023-09-27 11:20:32 -0500

    adjust ndim if else

--- lib/lmdist_source.r
+++ lib/lmdist_source.r
@@ -119,6 +119,7 @@
     for (i in 1:length(scree)) { scree_tot <- c(scree_tot, sum(scree[1:i])) }
     ndim <- min(which(scree_tot >= 0.8))
     if (ndim <= 1 | is.na(ndim) | is.null(ndim)) { ndim <- 2 }
+    else if (ndim > 10) { ndim <- 10 }
     corr <- round(cor(dist(pc$points[,1:ndim]), as.dist(lm_d), method="pearson"),3) # use correlation b/w PC & lmd dists as optimization func
   } else {
     lm_d <- NULL
